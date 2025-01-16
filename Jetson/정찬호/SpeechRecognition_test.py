1b2db7019590ce94fc12e2da2e45c7fdd4a5c138
chanhojeong <chano01794@gmail.com>
2025-01-16 17:27:13 +0900

    [test]_[DA]_음성인식 오프라인 라이브러리 실시간 테스트_S12P11B203-81
    Former-commit-id: 4e715a2ef31ae8bd3444b9bba0586203c64015c2

--- Jetson/정찬호/SpeechRecognition_test.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8271197e2164d159fff56ad806472bd0f12f23ce364841be2d770300d5a24fcc
-size 901
---
+++ Jetson/정찬호/[SUCC] SpeechRecognition_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:12fabdf86a7a0ba9a0f133ddd29043a205ab57c170ce099433c5a674be35ead6
+size 1022

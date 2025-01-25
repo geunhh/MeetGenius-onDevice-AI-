0adf136e1975d634e7bb66befc713e174c213a9f
rmsgnl11 <rmsgnl11@naver.com>
2025-01-25 17:53:48 +0900

    chore(ETC): venv 파일 제거
    venv 파일 제거.
    
    #S12P11B203-00
    
    
    Former-commit-id: bf2aeebd7518cc9fa4ed414e4900eb5f57af2e90

--- django/venv_django/bin/activate.csh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96262c4704ae43e657638cce567e0ddd0c7975c87d02491a89983b62b045c903
-size 965
--- fastapi/venv_fastapi/bin/activate.fish
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13b93a81d2486e11c0425a92005e7915e0d67dc7a0d2e59b98b35eab7968504d
-size 2249

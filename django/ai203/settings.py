6c397a4bb77e3d1a6707344e51187eefacb7c21f
장인영 <dlsdud0704@naver.com>
2025-01-26 00:03:37 +0900

    feat(BE): Maria BD 연결
    Maria DB 연결 중...시도 중
    
    #S12P11B203-176
    
    
    Former-commit-id: 4ffaa55d5dba8c077599f2c6f618c39396a7775a

--- django/ai203/settings.py
+++ django/ai203/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d15a4301dd1cfc1e797def1b7f55f571f63170860ac547f6e166b86bf0753be
-size 3506
+oid sha256:721a622a18621ed187bdc4b2493845c645c664e3e9c83b04be5a563e7f5efec6
+size 4001

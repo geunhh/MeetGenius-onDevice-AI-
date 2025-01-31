50c4ee16188283be3ec37904adcec5b91f857285
장인영 <dlsdud0704@naver.com>
2025-01-31 12:46:37 +0900

    chore(BE): Setting.py 수정
    setting.py 수정
    #S12P11B203-171
    
    
    Former-commit-id: 7d708092b1ccdf5f570e16478b723ab4b9c9920b

--- django/ai203/settings.py
+++ django/ai203/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50cccba55b9d4b91d002459277413cc04d341ff2ede324a8e7f668997db788b6
-size 5199
+oid sha256:1d09f3d3fb9de6517cb652db25e88b49b1d2d29f27f607d57b543c94dc88c550
+size 5286

0a2edff98acb8e77e9e68664e6f6f4472370aca0
geunhh <rmsgnl11@naver.com>
2025-01-31 12:45:34 +0900

    feat(BE): MariaDB 연결 구현
    MariaDB local 버전 연결 구현
    
    #S12P11B203-176
    
    
    Former-commit-id: 351067f580709db7d3b5fb21ba8156573ac916d5

--- django/ai203/settings.py
+++ django/ai203/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50cccba55b9d4b91d002459277413cc04d341ff2ede324a8e7f668997db788b6
+oid sha256:4ca53f82592ba7657f02f3c47f2f303b5023f9d6cbba327973b7acbdaff257ba
 size 5199

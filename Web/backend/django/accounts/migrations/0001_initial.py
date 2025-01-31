50f3bd700e41c6cf5b6b52461d8c5dc9cc48808f
장인영 <dlsdud0704@naver.com>
2025-01-31 16:56:43 +0900

    chore(BE): User 마이그레이션 수정
    User 마이그레이션 수정
    
    #S12P11B203-175
    
    
    Former-commit-id: 327d6d151d1fbb79e95ace4afa7333c963166ac0

---
+++ Web/backend/django/accounts/migrations/0001_initial.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d7db7fb37730cc3f50b258d42303fb91e380f62aa305008511bf72e0b9bbcbf
+size 2753
--- Web/backend/django/projects/migrations/0001_initial.py
+++ Web/backend/django/projects/migrations/0001_initial.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baf816eddbad49dbadec8ef446497406925fc0cb5d21443bd11fa78833845ff5
+oid sha256:d4ac8cf38240c2a98e99768c8321e723350d8f5a4ea62c5c1de0c492d25ec824
 size 3579

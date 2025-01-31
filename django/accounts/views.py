aa713307ed723a9d9e5451c4911d5e5195f0ccb7
장인영 <dlsdud0704@naver.com>
2025-01-31 15:15:27 +0900

    feat(BE): 로그인 로그아웃 구현
    urls, view, serializer, autentication 수정 및 추가
    
    #S12P11B203-183
    
    
    Former-commit-id: e879c96b056d606a56ddb4d1c6a8810529d02a6b

--- django/accounts/views.py
+++ django/accounts/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cd56d3f383b8a4c2a3492ebed9b3fc1740f1b7896ab32ee0c5bbb097250cbd2
+size 1288

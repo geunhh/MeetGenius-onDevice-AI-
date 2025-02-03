9e96e43f5f7d3d2affcc81c904354d77db643119
geunhh <rmsgnl11@naver.com>
2025-02-03 13:55:37 +0900

    style(BE): FastAPI 실시간회의 시작 및 종료 로직 구현
    회의 시작, STT, 회의 종료 로직 구현
    
    #S12P11B203-132
    
    
    Former-commit-id: 902de5cacfaec7a7f22c1e8908b90a2d0970a9f9

--- Jetson/backend/fastapi/app/api/endpoints/stt.py
+++ Jetson/backend/fastapi/app/api/endpoints/stt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3a1dc83bb5b3a9c043258ec6af63af05566ed3d37c1445e7001bed2c6464636
-size 2276
+oid sha256:7ffae6ef22038fc62f94e6a78c5cbdd4939cd42ab70c1fc3b8d42c1680c76e71
+size 2381

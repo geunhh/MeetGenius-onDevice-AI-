5883144072f247a1abe0a27650d737f9dff7adf8
연재환 <woghks1213@gmail.com>
2025-02-04 15:51:19 +0900

    feat(BE): FastAPI - 회의 시작 시 모델 로드 및 STT 시작 함수 정의
    FastAPI로 회의 시작 요청 시점에 모델 로드 및 STT 시작 함수 스크래치 정의 -> 실제 오디오 스트림 처리 작업 필요
    
    #S12P11B203-209
    
    
    Former-commit-id: 90846d4cc2100adcd1d8e6647a4a012abf890dfc

--- Jetson/backend/fastapi/app/api/endpoints/stt.py
+++ Jetson/backend/fastapi/app/api/endpoints/stt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae87dba7bba1b6382b4bf50c128f2bc4e3934c6addc0f18b42b368728d0e67b
-size 3114
+oid sha256:b1a64fc991a6c0572ee02814ea99ad47e0cbf38e10ba55f59d43c2b43ea15eda
+size 3921

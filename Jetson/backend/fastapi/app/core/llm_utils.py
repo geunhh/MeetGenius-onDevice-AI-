8ba3b9315fc615176339d66645f16e5acf5adc50
연재환 <woghks1213@gmail.com>
2025-02-05 00:25:30 +0900

    feat(AI): llm_utils.load_embedding_model() 수정
    로컬 모델 불러올 수 있게 코드 수정 필요
    
    #S12P11B203-209
    
    
    Former-commit-id: 7c6bf0a28fc3ea912c7015e06337718f3b60f34a

--- Jetson/backend/fastapi/app/core/llm_utils.py
+++ Jetson/backend/fastapi/app/core/llm_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89c1993f5c449c9ebd13dc9d89dc2d1437a78ad116a919f332f4e0800ab3e413
-size 1613
+oid sha256:0085a919d073ed045c0fdc62cd0813b4712bbc35fbbb98db1092292ce5af949d
+size 2960

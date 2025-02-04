47219f1caf00cf22f9b2ae122d6962b2d225c9dc
JAEHWAN <woghks1213@gmail.com>
2025-02-04 22:33:50 +0900

    feat(AI): llm_utils.py 작성 (모델 로드 및 언로드)
    회의 시작 시 모델 로드 / 회의 종료 시 모델 언로드 / 회의록 작성 시 모델 로드 등에 사용될 모델 관련 함수
    
    #S12P11B203-209
    
    
    Former-commit-id: 4b4dc851c5242d8103ffa07364480aabcbb48697

--- Jetson/backend/fastapi/app/core/llm_utils.py
+++ Jetson/backend/fastapi/app/core/llm_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4cc92d01eb66971dcc17ed5fb3d3ec4f10c1a20db72e9bee3ec847e4b14e71
-size 4422
+oid sha256:89c1993f5c449c9ebd13dc9d89dc2d1437a78ad116a919f332f4e0800ab3e413
+size 1613

523f9d4d78687301582f2d2611339baac1b43e1c
pgy <1222pgy@naver.com>
2025-02-05 17:30:28 +0900

    feat(AI): 요약 파이프라인 chromadb 연결 코드 추가
    chromadb에서 doc_id를 불러온 후, 그에 해당하는 원본 텍스트를 가져와 요약하도록 함.
    실제로 chromadb를 연결하지 않았으므로, 하드코딩으로 테스트 함.
    
    #S12P11B203-202
    
    
    Former-commit-id: f380a38e5905951d49ee2e430b6777160851ee9e

--- Jetson/backend/fastapi/app/api/endpoints/reports.py
+++ Jetson/backend/fastapi/app/api/endpoints/reports.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:988b8d55bc3bfa7e3e4476382a130289d7e9d3c44b03bca3374c49a93b18be63
-size 5460
+oid sha256:2a9aa3c58859b0766e3a5d323397fd8916395e4b885d66689720a7726b637dbb
+size 13570

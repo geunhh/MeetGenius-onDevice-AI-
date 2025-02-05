36f004527f0c5fadee5fb770336729602126f6d9
연재환 <woghks1213@gmail.com>
2025-02-05 17:32:08 +0900

    refactor(BE): stt.py 수정 (llm -> rag_model)
    기존의 app.state에 저장되던 llm을 rag_model 로 수정
    
    #S12P11B203-209
    
    
    Former-commit-id: e2f698401b440efc4800426d06e3a8d709f7167b

--- Jetson/backend/fastapi/app/api/endpoints/stt.py
+++ Jetson/backend/fastapi/app/api/endpoints/stt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef15ddb66954d81470b34531236f2566ebd55e5b36e82b37d76393f0a77bbbf
-size 4365
+oid sha256:05577374d6a2eb43f6c2bceafc1618a097234df684af83a06438d69d53437963
+size 4391

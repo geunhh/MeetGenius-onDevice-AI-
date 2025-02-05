403db960609f9598565abe44212ba44a21204ff5
연재환 <woghks1213@gmail.com>
2025-02-05 15:10:21 +0900

    refactor(BE): load_embedding_model() 함수 수정
    로컬 디렉토리 (.huggingface-cache/) 에 모델 저장 및 로컬 모델 로드 완료
    
    #S12P11B203-209
    
    
    Former-commit-id: ec09504ca8d8ef1f7a4676a7a091ae53ff902364

--- Jetson/backend/fastapi/app/core/llm_utils.py
+++ Jetson/backend/fastapi/app/core/llm_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0085a919d073ed045c0fdc62cd0813b4712bbc35fbbb98db1092292ce5af949d
-size 2960
+oid sha256:5e3d0b52ffd5a1315c48eec7d7e9b26c6f713c2012de0053f4fb41ed62867dd5
+size 2866

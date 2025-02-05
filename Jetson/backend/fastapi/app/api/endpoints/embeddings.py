7e40e9e3ea3b2cef1c08df7c7e538da925a808aa
연재환 <woghks1213@gmail.com>
2025-02-05 15:35:27 +0900

    feat(BE): 보고서 파일 추가 기능 구현(fastapi)
    보고서 파일 추가 및 전처리 & Django >> FastAPI
    
    #S12P11B203-207
    
    
    Former-commit-id: 87fe121969d377ceed3a3516c6efab613874bdd4

--- Jetson/backend/fastapi/app/api/endpoints/embeddings.py
+++ Jetson/backend/fastapi/app/api/endpoints/embeddings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb7adf2c6c3de092f6dae053269bff3bc94cb2fae7ae1d87b77c90fcf3efc39a
-size 548
+oid sha256:9da666a6fd44bc10cde82e47e4d83e98454472bd095c588486081605e25b0a10
+size 1168

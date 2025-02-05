4cbe7183670f9bac031f4da3435e3b2192bfec64
연재환 <woghks1213@gmail.com>
2025-02-05 14:50:44 +0900

    feat(BE): endpoints/stt.py 수정 모델 로드 및 언로드 메모리 최적화
    회의 시작 시 모델 로드 / 회의 종료 시 모델 언로드
    
    #S12P11B203-211
    
    
    Former-commit-id: 68d3ca822ccb1c8c9110482e7f83c26b002d51ba

--- Jetson/backend/fastapi/app/api/endpoints/stt.py
+++ Jetson/backend/fastapi/app/api/endpoints/stt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5127e5df038abcded427bfc671e28a54f583062287b4ad24aabd89bad5f99ef
-size 3861
+oid sha256:bef15ddb66954d81470b34531236f2566ebd55e5b36e82b37d76393f0a77bbbf
+size 4365

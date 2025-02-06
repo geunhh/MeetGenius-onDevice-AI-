50fbaa4b7d5c6eddd5eeddca878212a44dd479a8
장인영 <dlsdud0704@naver.com>
2025-02-06 16:47:33 +0900

    chore(BE):프로젝트 정보 불러오기 회의 생성시 프로젝트 정보 불러오기 #S12P11B203-194
    Former-commit-id: 99f4cadcb4541b0227679a474a4e58498955a73f

--- Web/backend/django/meetingroom/serializers.py
+++ Web/backend/django/meetingroom/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbdde071d434e91a86b07b3dc4bb7ed7c5e67e24128280ac4614b8faa960f8b7
-size 1408
+oid sha256:35f9a176a9221fc91369bccdbb61acb62320836e6ead074bd1f05bb803ce66fe
+size 2250

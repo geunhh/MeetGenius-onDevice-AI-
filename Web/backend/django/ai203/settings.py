7472dd6c13f08c706f9d81f95166435c501c58de
장인영 <dlsdud0704@naver.com>
2025-02-06 10:31:01 +0900

    chore(BE):인증권한 변경
    settings.py 수정
    
    #S12P11B203-171
    
    
    Former-commit-id: bbc70d555afd29233fef1794f490303df805e02c

--- Web/backend/django/ai203/settings.py
+++ Web/backend/django/ai203/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352e6adc7fe2f571fb1ba69e56ef426e73ac2d7b42477628d1765328e3f971cb
-size 4898
+oid sha256:38cac3eb5c72ca24cbb70048a6d9ad015ea3ebe51f641ffa7746a2eeda1c9bc0
+size 4823

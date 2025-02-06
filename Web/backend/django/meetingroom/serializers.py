b6d25787dcf12099f585fcca21c0778f1d210590
장인영 <dlsdud0704@naver.com>
2025-02-06 15:27:59 +0900

    chore(BE): 예약된 전체 회의 조회 기능
    회의실 별로 조회가능, 주간별 조회가능
    
    #S12P11B203-199
    
    
    Former-commit-id: 5232658ecdf63c8e19e57522ede81a96249fc7f0

--- Web/backend/django/meetingroom/serializers.py
+++ Web/backend/django/meetingroom/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64cfc00ce8d1f3c85accfede445834dd2a48f38594053c75f34c16c3a097b4c
-size 1193
+oid sha256:cbdde071d434e91a86b07b3dc4bb7ed7c5e67e24128280ac4614b8faa960f8b7
+size 1408
--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dca9537b98fff8f082817eeebe4482e3a4602fe75acfecf94ae8400cd84a4621
-size 2131
+oid sha256:a2075ce2cf61abfe4aa89ac072c6a2c6047ddf6dad7762bc65d7dcc987b7626c
+size 2922

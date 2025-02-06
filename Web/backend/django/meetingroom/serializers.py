142bc730ad7c9a8a9323b1c2cfe8e671682c8b7c
장인영 <dlsdud0704@naver.com>
2025-02-06 10:43:22 +0900

    feat(BE): 예약된 전체 회의 조회 기능
    예약된 전체 회의 조회 기능
    
    #S12P11B203-199
    
    
    Former-commit-id: 7f1e1fe922b1619c7c5132f40c92e6a9ba8f772a

--- Web/backend/django/meetingroom/serializers.py
+++ Web/backend/django/meetingroom/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a28c7d8318c0d8d1a685f4b9b53def7a753b75a057252928767f04efeec67658
-size 1352
+oid sha256:d64cfc00ce8d1f3c85accfede445834dd2a48f38594053c75f34c16c3a097b4c
+size 1193

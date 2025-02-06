38e800477a4c6c5db5fb9efa7f694956abe1cbf3
장인영 <dlsdud0704@naver.com>
2025-02-06 09:34:02 +0900

    chore(BE): agenda 모델 수정 오타 수정 #S12P11B203-193
    Former-commit-id: f4a0b71d6540716c22c9ecb85f829c45dd230636

--- Web/backend/django/meetingroom/models.py
+++ Web/backend/django/meetingroom/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ff91d42d50018e29487abb404ebec5624bc97ae0cb887fb1fbda511e2e7bc7c
+oid sha256:896f097e4d1d2b77a017f2eb0b8eff9f3a5c9471e8fba82b91a02e192d0f7fee
 size 1567
---
+++ Web/backend/django/meetingroom/serializers.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a28c7d8318c0d8d1a685f4b9b53def7a753b75a057252928767f04efeec67658
+size 1352

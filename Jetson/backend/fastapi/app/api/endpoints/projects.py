e3d3022c6f14cd8e4ec2e954ed2435b21fd5a670
연재환 <woghks1213@gmail.com>
2025-02-07 00:57:55 +0900

    feat(BE): 프로젝트 관련 문서 삽입 엔드포인트 정의
    프로젝트 관련 문서 삽입 시 임베딩 후 ChromaDB 저장
    
    #S12P11B203-213
    
    
    Former-commit-id: f4ea9b0fabf974dff7b4f34939e356bee14de2a7

---
+++ Jetson/backend/fastapi/app/api/endpoints/projects.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5c1c260a6c88c91dd1ea2b4b48a972b0ee5f13af8fb974ab1cf91640c932019
+size 5123
--- Jetson/backend/fastapi/app/core/chromadb_utils.py
+++ Jetson/backend/fastapi/app/core/chromadb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23e6a7b79378c9fa1f7ee8001badde69245e4039be9f0c7735a10975b8e3b6fe
-size 3517
+oid sha256:a81d5a373c487b686de2804842dbeeeb38e399b03623d188b5eea72aeafe6f09
+size 4149

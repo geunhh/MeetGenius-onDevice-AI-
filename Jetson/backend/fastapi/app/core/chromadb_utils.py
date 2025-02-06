c6a8f1b4b5f256015fb215cff9d883d1c6259fca
연재환 <woghks1213@gmail.com>
2025-02-06 23:26:20 +0900

    feat(BE): endpoints/meetings.py 생성 및 prepare_meeting() 정의
    FastAPI 엔드포인트 정리 및 회의 준비 엔드포인트 함수 작성
    
    #S12P11B203-215
    
    
    Former-commit-id: 73c248704288884e13c04ae8d95bcfb2b432b5d8

--- Jetson/backend/fastapi/app/core/chromadb_utils.py
+++ Jetson/backend/fastapi/app/core/chromadb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fa03d4f6313474b356540bdac0a2963a6ae91fa07086f8c479b399d76c331a
-size 3919
+oid sha256:23e6a7b79378c9fa1f7ee8001badde69245e4039be9f0c7735a10975b8e3b6fe
+size 3517

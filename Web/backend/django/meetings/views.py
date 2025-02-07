a85d4dc9cb07931e6fdaf52329f97647a4f2d0a0
geunhh <rmsgnl11@naver.com>
2025-02-07 20:48:38 +0900

    feat(BE): 실시간 회의 시스템 구현 - 1
    실시간 회의 시스템 1차 수정 구현.
    
    #S12P11B203-212
    
    
    Former-commit-id: 3440a3b7cf4afcf0ae40bee68bae90315ca83787

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aee47b5897a360209f60a1cc83200b76442bb1dcf58bc5dc99dbe52b86a1878
-size 2931
+oid sha256:00f4134696459bba8cea7a9d1a1ac3d7902997f9be0bb86a50d529444202aea9
+size 9434

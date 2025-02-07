e4a349f12b57909bd65173f4e27de23b198198b6
장인영 <dlsdud0704@naver.com>
2025-02-07 13:17:18 +0900

    chore(BE): 회의예약기능 회의 예약 시간 입력 데이터 폼 변경 #S12P11B203-194
    Former-commit-id: ff989536c7d1218b63618acf952c803755f8888f

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8803ef4a6d19b71e0982dd08e98ec00e9627f2161f636ca2c1045e4255d20551
-size 4365
+oid sha256:d18e74c01759be1ea70e4adb512a8b401d2b4a8f9d3aabd99a541f0fe6814eec
+size 5143

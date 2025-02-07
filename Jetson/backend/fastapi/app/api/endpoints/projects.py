db01e1b5d453633772385fd9bfb4eb22b1a8cc1b
연재환 <woghks1213@gmail.com>
2025-02-07 09:40:28 +0900

    feat(BE): 프로젝트 관련 문서 삭제 엔드포인트 정의
    프로젝트 관련 문서 ID 수신 받아 삭제
    
    #S12P11B203-213
    
    
    Former-commit-id: e2fe416ed60241541a099c4da7cdbb553ccb0bfe

--- Jetson/backend/fastapi/app/api/endpoints/projects.py
+++ Jetson/backend/fastapi/app/api/endpoints/projects.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0f5047e14d93d12e752e5719db3ca9fa4333bf5bb4221265ee09dae260a6b9e
-size 3761
+oid sha256:c4453a548cc6c335431dabb93e4ebee89d89de7f60285771f9ca0fe60ce9a0e1
+size 3956

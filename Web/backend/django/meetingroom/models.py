be2896af15975a276e1d18d2f3035dae4cc13aa1
장인영 <dlsdud0704@naver.com>
2025-02-07 12:56:07 +0900

    feat(BE): 회의예약기능 회의 예약 기능| #S12P11B203-194
    Former-commit-id: aceaa232750a3bd422885fb4ec97a879515bc897

--- Web/backend/django/meetingroom/models.py
+++ Web/backend/django/meetingroom/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:896f097e4d1d2b77a017f2eb0b8eff9f3a5c9471e8fba82b91a02e192d0f7fee
-size 1567
+oid sha256:97bfd28a0e4dbe2b876c2b1611887fbaf9dbbd78a9788e801eccfd769c8a83b0
+size 1569
--- Web/backend/django/meetingroom/serializers.py
+++ Web/backend/django/meetingroom/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f9a176a9221fc91369bccdbb61acb62320836e6ead074bd1f05bb803ce66fe
-size 2250
+oid sha256:688eff6c6447401847c1de5ff19e3da7c923cc0175a32f517f2e365a7ea0b4d2
+size 1891

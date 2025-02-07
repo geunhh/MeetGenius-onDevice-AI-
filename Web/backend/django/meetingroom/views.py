859fcc071b119807e748de9f8c77ed6fc230d7ff
장인영 <dlsdud0704@naver.com>
2025-02-07 16:32:41 +0900

    feat(BE): 회의 안건 CRUD
    회의 삭제
    
    #S12P11B203-198
    
    
    Former-commit-id: b39bb4d493fa9f22fbb4600030176ee398ba127a

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e6b0470a896f0539fc877aaace108e839ce316445149deb088658a4f792f3bb
-size 6054
+oid sha256:2e545cea42cf83e9cc6f14251d4519143717b2760e22e29de7cc1991d016790d
+size 7438

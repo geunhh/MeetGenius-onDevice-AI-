beb498589f279a80c917059653b39c30d0b15cdc
연재환 <woghks1213@gmail.com>
2025-02-08 15:37:31 +0900

    refactor(BE): 다음 안건 엔드포인트 반환 모델 적용
    기존의 일반 json 형식의 반환값을 NextAgendaOut이라는 모델을 정의하여 수정
    
    #S12P11B203-223
    
    
    Former-commit-id: 718023cd0096f7fa2ab644450d68d1674124702e

--- Jetson/backend/fastapi/app/api/endpoints/meetings.py
+++ Jetson/backend/fastapi/app/api/endpoints/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b05fa0bf578d084bcc2c05ed3055074df293239b3cfc4d9441cc5b1fa944908
-size 6745
+oid sha256:1f1901b8151080698070e89a67fd33b50a736a3becdcebae30d1b6475e2b5e02
+size 6767

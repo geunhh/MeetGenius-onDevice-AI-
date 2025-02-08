9c10136ee79f90555ee28fae6dabff5f2cfd6ec7
장인영 <dlsdud0704@naver.com>
2025-02-09 00:53:08 +0900

    feat(BE): 회의 수정기능 회의정보 수정 및 재알림 기능 구현완료
    #S12P11B203-196
    
    
    Former-commit-id: 1ee2fdac406c940ae4abccc184a82da215441bbd

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:738b2531b0583ac6ffae75733275171cd88e639ffa8b8d6773305f451c8311eb
-size 7799
+oid sha256:23a55e8624737420aa71eacd7106a199fc6fdd873922e53ab6b01f94cedaf10f
+size 14155

fdce48c1b85c723e81e8aeb9f1cff7dd439317ed
연재환 <woghks1213@gmail.com>
2025-02-08 15:33:06 +0900

    fix(BE): meetings.prepare_meeting 반환 부분 수정
    pydantic 모델 사용으로 인한 반환값 설정 수정
    
    #S12P11B203-215
    
    
    Former-commit-id: 482cfa2d8ad9a4795d37d2f37d647c5aa8799b32

--- Jetson/backend/fastapi/app/api/endpoints/meetings.py
+++ Jetson/backend/fastapi/app/api/endpoints/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae3fc825c6265f28653df87706105ae60f5722f702ed59cbcf6b36cc98e7bb9d
-size 6754
+oid sha256:6b05fa0bf578d084bcc2c05ed3055074df293239b3cfc4d9441cc5b1fa944908
+size 6745

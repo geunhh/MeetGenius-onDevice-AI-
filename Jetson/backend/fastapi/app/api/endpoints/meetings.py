c286b43a0dfed51f00603ba7e460067133be23ab
연재환 <woghks1213@gmail.com>
2025-02-08 15:28:02 +0900

    refactor(BE): 회의 준비 엔드포인트 파라미터 수정 및 예외처리 추가
    endpoints/meetings.prepare_meeting() 함수 파라미터 AgendaList로 변경
    
    #S12P11B203-215
    
    
    Former-commit-id: 8fde2811ac005bfa2fb9297c1f8cae56b549410e

--- Jetson/backend/fastapi/app/api/endpoints/meetings.py
+++ Jetson/backend/fastapi/app/api/endpoints/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d56435e3ba6e9a10b82c87a8210480fa87905d23e551987c91ff167b1cb1265
-size 6447
+oid sha256:ae3fc825c6265f28653df87706105ae60f5722f702ed59cbcf6b36cc98e7bb9d
+size 6754

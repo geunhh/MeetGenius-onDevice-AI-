480009067fae7e664d1a1c66f5cf1136947d6a55
연재환 <woghks1213@gmail.com>
2025-02-08 15:35:30 +0900

    feat(BE): NextAgendaOut 모델 정의
    endpoints/meetings.next_agenda() 함수 반환 모델 정의
    
    #S12P11B203-223
    
    
    Former-commit-id: 18c731870af5eeee196ba5e35b3fb595eda09a1e

--- Jetson/backend/fastapi/app/models/meetings.py
+++ Jetson/backend/fastapi/app/models/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b617d1929602199fd62d8930a426f21fb44c25f2b8ec30588e4ee96c377daa92
-size 627
+oid sha256:2b939287e62b11ea4fdea12649844fb93ba007c57ed96aad8bb0403cceb6be0c
+size 846

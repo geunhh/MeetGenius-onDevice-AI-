4e8ad1f4c24e36a2cf47c2c032e90e7bf18049eb
geunhh <rmsgnl11@naver.com>
2025-02-10 10:18:53 +0900

    feat(BE): 다음 안건 로직 구현
    Redis-RAG 문서 중복 방지 로직 추가, next_agenda : 다음안건 기능 구현
    
    #S12P11B203-241
    
    
    Former-commit-id: 280261366732525267bcfea7411d1906864cfb43

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2678f3cc77d61a55d0b0acfa372adf3adf21de82617519f8e73ca0212707804
-size 19018
+oid sha256:ee76f7516613bdd469f376a4e0eabdbe318dcefafa73d9572da69161ecf60843
+size 22336

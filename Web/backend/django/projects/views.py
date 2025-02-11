64a16910834a627cca6e29a9fa4d5b76f40dfac5
정유진 <tntn6981@naver.com>
2025-02-11 17:14:09 +0900

    chore(FE): 프로젝트 문서 업로드 수정
    프로젝트 전체 문서 불러오도록 수정
    
    #S12P11B203-265
    
    
    Former-commit-id: 325f2557a052c0afddec2b0d63db3f8aa8314558

--- Web/backend/django/projects/views.py
+++ Web/backend/django/projects/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3f7131f2146d293d802ecf06ddd1787ae4847d8ababa8a24b881e98d8480936
-size 7847
+oid sha256:81c52f5aab55ecb980b85cd56f53a8c56fbd73e395ab0b498ec15030debefcda
+size 7825

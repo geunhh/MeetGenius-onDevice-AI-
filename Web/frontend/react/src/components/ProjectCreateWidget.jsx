4f011b365aabfb72145d630bfad71346c171cc87
정유진 <tntn6981@naver.com>
2025-02-11 16:00:11 +0900

    feat(FE): 프로젝트 페이지 구현 완료
    프로젝트 생성, 조회, 수정, 파일추가 구현 완료
    
    #S12P11B203-227
    
    
    Former-commit-id: 403cd434c87c7ff29f13cd309b61a0de2668bb40

--- Web/frontend/react/src/components/ProjectCreateWidget.jsx
+++ Web/frontend/react/src/components/ProjectCreateWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65c7a5939d7d86aed87b27e05e05dae331ef1be6620f0d5d7ed350f5eb9b11d8
-size 9157
+oid sha256:1ab7f33899db0094d59c330d234caf8f9e5ea4d3d47d6fc9269a5d124c4c7f94
+size 9299
---
+++ Web/frontend/react/src/components/ProjectDetail.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a68bd97e0b1175dda517756146b97abd984311fa011215aa749566a51b2d34d
+size 12559

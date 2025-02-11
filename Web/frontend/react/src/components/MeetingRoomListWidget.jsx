61a8453e03ed42bd50296d6b02250212545d5413
장인영 <dlsdud0704@naver.com>
2025-02-11 16:11:54 +0900

    feat(FE): 회의내역 확인 회의내역 확인 기능, 자잘한 오류 있음 #S12P11B203-263
    Former-commit-id: 381b458e66b5bf5acde7e7e9ea4a4bae095f7516

--- Web/frontend/react/src/components/MeetingRoomListWidget.jsx
+++ Web/frontend/react/src/components/MeetingRoomListWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9db4c7af37abc4f0a1956dc052b19d3ee5f7a4b9f7975a5f20cceb1095622b
-size 3101
+oid sha256:f341d4ce0290c542b6fbfc4794c8a38364bbdcdd4f3f2a3a77c34a9077a09a1d
+size 3225

daefb90ad281f443d8aa6d89f3689453deb789f5
정유진 <tntn6981@naver.com>
2025-02-11 17:08:34 +0900

    chore(FE): 프로젝트 문서 업로드 수정
    프로젝트 전체 문서 불러오도록 수정
    
    #S12P11B203-265
    
    
    Former-commit-id: 19ff0d24cb5d9c1e1145f88132bf0bfc43dd30b8

--- Web/frontend/react/src/components/ProjectDetail.jsx
+++ Web/frontend/react/src/components/ProjectDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a68bd97e0b1175dda517756146b97abd984311fa011215aa749566a51b2d34d
-size 12559
+oid sha256:2810a25fcb511d0dc07fa9b3e5fdf36653a93c8cd6414d9b5a6d4851db761103
+size 13489

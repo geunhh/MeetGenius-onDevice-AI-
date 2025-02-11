bebd2d1bad966a8addd2aee33570b37fb4de6fbc
연재환 <woghks1213@gmail.com>
2025-02-11 16:55:49 +0900

    fix(AI): rag 테스트 및 파이프라인 수정
    수정파일: routes/meetings.py services/rag.py utils/chromadb_utils.py utils/llm_utils.py
    
    #S12P11B203-249
    
    
    Former-commit-id: aee3389e9f5a273765024b309bcde810a2d476c0

--- Jetson/fastapi/app/utils/chromadb_utils.py
+++ Jetson/fastapi/app/utils/chromadb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f61b89743dd76a546754f00e66ce248784a4e355586a5d8b157459ae1c233918
-size 5655
+oid sha256:a3e0799985ce92b62ff530b46d2871b3caedc1bc1f73cb1a981d174bd2ba4f81
+size 5522
--- Jetson/fastapi/app/utils/llm_utils.py
+++ Jetson/fastapi/app/utils/llm_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f037bc3df5fa008f5431d7671b609ff38ba7fa8932650e473b8e1cd073ecdcba
-size 4407
+oid sha256:78d0e8660a12b59aeb96de35b935132ded842ae5a16bc257059fd5cd975333ed
+size 5207

82dd19454a45ef5ff75113e86688653b77fbe536
연재환 <woghks1213@gmail.com>
2025-02-11 14:59:14 +0900

    fix(BE): 다음안건 엔드포인트 테스트 및 코드 수정
    수정 파일: routes/meetings.py utils/chromadb_utils.py
    
    #S12P11B203-249
    
    
    Former-commit-id: 13dc30c80ff12e81d287894fdce977b201f29407

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:179cf566c731de3ae3ba35bc17af6f89c752af84d6eb165a2916e91c5b266f0c
-size 10568
+oid sha256:12fc5da8b237b723d03675466eeb0ec0e6e853e948178f77502914da09e79c76
+size 10604

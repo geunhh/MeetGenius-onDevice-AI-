349f44f0874a812582872c9a72ff8f7b4a6ac54d
연재환 <woghks1213@gmail.com>
2025-02-13 13:54:52 +0900

    feat(AI): 프롬프트 엔지니어링
    엔지니어링 중
    
    #S12P11B203-235
    
    
    Former-commit-id: 6378f65518e56c8f0f54a35660d4d69102d34b78

--- Jetson/fastapi/app/api/routes/tests.py
+++ Jetson/fastapi/app/api/routes/tests.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c0ffdeb87864330a92bece4fb4e4f76aabcedacd082315c443db697e6123d67
-size 524
+oid sha256:06302f7deb8eac4f8e84a4b94aaf4617e20a272edb476bdd8238824735f9aa8b
+size 1701

279176fb967dca3dd9fdb8db81bec9304818b074
장인영 <dlsdud0704@naver.com>
2025-02-14 06:12:00 +0900

    feat(BE): 프로젝트 삭제 api 개발 프로젝트 수정, 삭제 시 권한 인증 확인 S12P11B203-285
    Former-commit-id: e4a015c09bee35d7dfeb7d7636b5f16cdd80d1da

--- Web/backend/django/projects/views.py
+++ Web/backend/django/projects/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c52f5aab55ecb980b85cd56f53a8c56fbd73e395ab0b498ec15030debefcda
-size 7825
+oid sha256:1b7191a1e0b625970c18e07a058f1e2b5550c6ec40d6bfb4c94cf9a9df3359b6
+size 9051

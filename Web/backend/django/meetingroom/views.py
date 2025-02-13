fe8e5edb9a3755a47a019188d9231fd604884b1d
장인영 <dlsdud0704@naver.com>
2025-02-13 17:59:16 +0900

    chore(FE): 회의별 색깔 변경 회의별 색깔 변경 #S12P11B203-250
    Former-commit-id: 55cdf5eccf5c9fd0298101fe1242403a651a6a63

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d86f268f4bd63cea2cd9651679626605b741822f2cf6610765f4042a265afb58
+oid sha256:1cb0b4e7dc6b818689313fa3df13b21cc5114c8386b28d6d1baa650b63e2b54c
 size 15894

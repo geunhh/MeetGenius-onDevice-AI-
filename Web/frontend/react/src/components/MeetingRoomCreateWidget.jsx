439bc5531261c1db9c7452acf08f2485d5a22ce3
장인영 <dlsdud0704@naver.com>
2025-02-13 16:23:36 +0900

    fix(BE): 프로젝트 참여자 목록, 회의 참여자 목록 참여자 목록 입력 재설정 #S12P11B203-261
    Former-commit-id: 92e07aae5493d8fb023ec43f565e6a569d7f17ef

--- Web/frontend/react/src/components/MeetingRoomCreateWidget.jsx
+++ Web/frontend/react/src/components/MeetingRoomCreateWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b142ee834634ca31e44de0c7444816a2635ef13ae23e1356492d941c5d7ffb50
-size 10675
+oid sha256:1478d6ad84dbb7edf674c6ca98724d3af06b07471ae2d9896907b5bd2598d74b
+size 10630

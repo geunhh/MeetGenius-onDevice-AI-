3a21b14a1e27b5b0da641b8a3dbef020e673d75e
정유진 <tntn6981@naver.com>
2025-02-14 22:16:21 +0900

    chore(FE): 머지를 위한 커밋중
    실시간 회의록 기능 구현했었음
    
    #S12P11B203-289
    
    
    Former-commit-id: 3757e21966369eca03ec4d8b4c6b692bf04627b2

--- Web/frontend/react/src/api/axiosInstance.js
+++ Web/frontend/react/src/api/axiosInstance.js
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2aafdbd0b0bf660f8cf666c35d352ad66d9f1dc2e21843824d89ed37e75c2a67
-size 1426
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_APP_BASEURL;
+// ✅ Axios 인스턴스 생성
+const axiosInstance = axios.create({
+  
+  baseURL: baseURL, // Django 백엔드 API URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+axiosInstance.interceptors.request.use((config) => {
+  const authToken = localStorage.getItem("authToken");
+  if (authToken) {
+    config.headers["Authorization"] = `Token ${authToken}`;
+  }
+  return config;
+});
+
+export default axiosInstance;

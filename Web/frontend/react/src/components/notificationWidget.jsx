000ae7fb40c5e86ad24a1bac9fdc398794d89f90
장인영 <dlsdud0704@naver.com>
2025-02-14 15:04:06 +0900

    chore(FE): 알림 비동기적으로 처리 알림을 리덕스로 관리 #S12P11B203-280
    Former-commit-id: fb0c5fe2fa641d1215f7b8fcfb812e72cdac583e

--- Web/frontend/react/src/components/notificationWidget.jsx
+++ Web/frontend/react/src/components/notificationWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d8cf5602be820492a47a26e49080d18041d949e4f6c15b80d66ab1e458da95b
-size 5222
+oid sha256:1cabae58e762a7dc7253a9943c670377020e0734ae0eadb3367892753ad94f82
+size 5072

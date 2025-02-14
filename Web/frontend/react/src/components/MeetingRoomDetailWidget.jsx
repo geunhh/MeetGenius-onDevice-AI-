47881e25c77ec339588bc84d435349550d680bc8
장인영 <dlsdud0704@naver.com>
2025-02-14 22:58:33 +0900

    Update Web/frontend/react/src/components/MeetingRoomDetailWidget.jsx
    Former-commit-id: 1abba135082550baa7dd23664580f9dabd4b02d3

--- Web/frontend/react/src/components/MeetingRoomDetailWidget.jsx
+++ Web/frontend/react/src/components/MeetingRoomDetailWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:858804a09631f8b5b44541c70ef8ddc2b73faca6fedd6838302cf41a1a80f3c0
-size 5984
+oid sha256:f341325663246bcace8e60a4db9b1e0edffbf83f25925d60a8b830d6378363cc
+size 5708

0f13213f550d73303b8091a0fa5f7b050680ea0c
장인영 <dlsdud0704@naver.com>
2025-02-14 14:29:38 +0900

    chore(FE): 회의 예약 회의 예약 수정, 리덕스로 변수 관리 #S12P11B203-261
    Former-commit-id: 1ad6a52b3404a4993a92f65fbf0b1d1ecce72294

--- Web/frontend/react/src/components/MeetingRoomListWidget.jsx
+++ Web/frontend/react/src/components/MeetingRoomListWidget.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7db6b146f3857f467aa71cd9c1c0a3b93b30631929150187fb32adce07e8eb4e
-size 3180
+oid sha256:f65009df8d87681e7885f7a234c11fabf133b148c84d2400cb89713e3e3cdeeb
+size 3182

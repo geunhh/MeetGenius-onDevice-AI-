b560aadabb383ab01640d4bb0517599daa457378
geunhh <rmsgnl11@naver.com>
2025-02-14 13:06:34 +0900

    feat(BE): 로그인 중인 유저 정보 조회
    로그인 중인 유저 정보 조회
    
    #S12P11B203-287
    
    
    Former-commit-id: ac8bf02f5a317e6451cf53d14b6964aff12d431f

--- Web/backend/django/accounts/views.py
+++ Web/backend/django/accounts/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50ca4c323abe23d00fd17b1b262ff85bb2104259b7822c489e381cd16c6db47a
-size 4446
+oid sha256:f58a8f0b1996a0a18a1ac41bc5a47604fa3a41d9e882a52712f46667688f39d6
+size 4688

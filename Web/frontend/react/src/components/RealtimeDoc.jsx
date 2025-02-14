85a2cf336cdae2c60d7fa635900983de5288b403
정유진 <tntn6981@naver.com>
2025-02-14 15:30:21 +0900

    feat(FE): 실시간 회의록 기능 구현
    실시간 회의록 기능 구현
    
    #S12P11B203-289
    
    
    Former-commit-id: 0b160248baa2733c578e3f3ca7d854a772d1220a

--- Web/frontend/react/src/components/RealtimeDoc.jsx
+++ Web/frontend/react/src/components/RealtimeDoc.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0577382c30e38109f1686c3432f465a08a494746168adde6a453f4ae06caf3e
-size 680
+oid sha256:b6db0909148e61db2b6eac6e5f13821ac8126741cce24b4f828572dcc7cd0fcf
+size 693
--- Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
+++ Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d1573974a0611c834f5cf35e2f694fd64a91944aab03faf26192009852ef31f
-size 4848
+oid sha256:9b40c2883b43f8ab679f77fb5bc7b2e0d20ffdbbad582acd0ce7fc767f8714c8
+size 5330

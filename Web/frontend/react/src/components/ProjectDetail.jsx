50df9a64d983f3d37d6ed2355ba97f51508e9824
정유진 <tntn6981@naver.com>
2025-02-16 06:56:37 +0900

    feat(FE): 회의록 컴포넌트와 회의관련 문서 컴포넌트 구현
    회의록 컴포넌트와 회의관련 문서 컴포넌트 구현
    
    #S12P11B203-292
    
    
    Former-commit-id: 542a2a14eec7b547d2bc57fac0b5ea34e06023fe

--- Web/frontend/react/src/components/ProjectDetail.jsx
+++ Web/frontend/react/src/components/ProjectDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db2428ca41546194224a656810927b759ce5fc94ad61583f19dc0ca3dc27bc6e
-size 13473
+oid sha256:91d379012cb18ab3b774f59399473d413cf61b0ffbeca55aedce2c9f48793113
+size 14192
--- Web/frontend/react/src/components/RealtimeDoc.jsx
+++ Web/frontend/react/src/components/RealtimeDoc.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6db0909148e61db2b6eac6e5f13821ac8126741cce24b4f828572dcc7cd0fcf
-size 693
+oid sha256:34b8d77d44459f1e6f861ab33c2771baf581719874cbc06f89fd6e2139a1e8a4
+size 6312

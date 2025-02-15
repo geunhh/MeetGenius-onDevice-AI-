67743331b1018baf5eeddd4fed2a9116a6b0d848
정유진 <tntn6981@naver.com>
2025-02-16 05:46:04 +0900

    feat(FE): stt type별로 구분해서 실시간으로 띄우기
    stt type별로 구분해서 실시간으로 띄우기
    
    #S12P11B203-294
    
    
    Former-commit-id: f5126206ecb1058b43b680931a34ec74a26f1384

--- Web/frontend/react/src/components/RealtimeNote.jsx
+++ Web/frontend/react/src/components/RealtimeNote.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddbfe37e8991dc828df2cd303c8d4ee72abecae989824e5d313ad1d4b1ed22e5
-size 9620
+oid sha256:0e9715fb65770f738ab286edd2a5ae67cc45a204c328526970eebe989cec0701
+size 10589

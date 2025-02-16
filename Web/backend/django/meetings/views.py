c83bfd78365efebea235127f507041d2c3670c52
정유진 <tntn6981@naver.com>
2025-02-16 22:03:45 +0900

    fix(FE): meeting_state 상태 플로우 통일
    meeting_state 상태 플로우 통일
    
    #S12P11B203-000
    
    
    Former-commit-id: 6bab069e83300bb1638e2be1b1ba473ff17fbeaf

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d82ac902ab3cc9b8699f06f857f7dbf98739fcaddc63dc4ff76d1615534e268
-size 35988
+oid sha256:dde0f1e3a6d798c7258df7b3aaeba794aae96ce2e6a0ec03aeaeba86a34e239c
+size 36152

45e0a837b3e6edbd53910ceb11644010761f1f4f
장인영 <dlsdud0704@naver.com>
2025-02-17 01:13:29 +0900

    fix(BE): Mymeeting 조회하기 사용자가 참여하고 있는 미팅 조회하기 S12P11B203-273
    Former-commit-id: 35596a0ff41326915c638521d64a0a6d3687c1ef

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf7acbc0afed953337ac682f23880daf9b9b9bfa7c17e5480544aa8890fa1334
-size 23514
+oid sha256:1c1dc148a93c41407e3ac8f694b9bf4e46c07faa85da0fa11c43db94b9c0e39a
+size 23480

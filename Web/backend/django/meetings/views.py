5832128d4cd707daa71306686d36f39c75c1f0d7
정유진 <tntn6981@naver.com>
2025-02-16 17:11:51 +0900

    fix(FE): 실시간 회의 페이지 에러 수정
    실시간 회의 페이지 회의록 페이지 에러 코드 수정, FastAPI 연동 코드 주석처리
    
    #S12P11B203-000
    
    
    Former-commit-id: 2148c77da27ea2226e8fa2512770eea41624fa40

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1787f55c804981bccfb7a76ed953902622351d46391d5e0d5bc67f42b61c0e63
-size 35616
+oid sha256:4bcdfa0a74a7b9fe0ed2557a33f401d8016c1e3157c7929fcb97e079df57ff7d
+size 35794

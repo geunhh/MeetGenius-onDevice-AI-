55ec03b9b733ea3ebdd3b1df232a327fbbdce181
rmsgnl11 <rmsgnl11@naver.com>
2025-02-16 21:23:32 +0900

    require
    Former-commit-id: 088b0287239d365a634b9d088200595b8960eaf0

--- Web/backend/django/meetingroom/views.py
+++ Web/backend/django/meetingroom/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91e5ab3087633757643abe54241aa379be753eb0a8aa3de1599f53c371ca31b1
-size 23150
+oid sha256:0ec7f0ef5799be772ca337a0b5cb8e88747c49af59f91be8889e3c9403679b62
+size 23517

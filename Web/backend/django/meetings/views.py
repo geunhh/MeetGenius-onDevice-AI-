ea515ecfda3f34c8520a5bce6726eba175e0ba22
정유진 <tntn6981@naver.com>
2025-02-16 17:47:18 +0900

    fix(FE): Redis Pub/Sub 회의 상태 메시지 추가
    meeting_state 추가
    
    #S12P11B203-000
    
    
    Former-commit-id: 612b1d93e65345a59cf4506bae1299a658143dde

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bcdfa0a74a7b9fe0ed2557a33f401d8016c1e3157c7929fcb97e079df57ff7d
-size 35794
+oid sha256:9ef4acba2f7c0d9f8a55150801fecab3dbe52aafcb2ea2c3be1eeaaf01d9b767
+size 35972

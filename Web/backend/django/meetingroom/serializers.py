ffeb87d38c2a97af2d742e858c4995a8339493f8
rmsgnl11 <rmsgnl11@naver.com>
2025-02-16 23:12:09 +0900

    fix(BE): RAG 데이터 처리 수정
     RAG 데이터 처리 수정 - 에러 해결
    
    #S12P11B203-000
    
    
    Former-commit-id: a7eefcd7308646ba91e3de60e5af58cff1e3a0ef

--- Web/backend/django/meetingroom/serializers.py
+++ Web/backend/django/meetingroom/serializers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1454d7a8b860f678873e622a4976894d2a1de0b9c0a3276fde922ab77ddbe526
+oid sha256:27e83f9ab803608bebc9fdfb4d978eb70bc6abfa70799acbe2d127cd1b8131a3
 size 3155

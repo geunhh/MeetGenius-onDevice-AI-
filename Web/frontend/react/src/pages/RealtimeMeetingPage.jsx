ac3e4fe53f78d153510db28ed6bed6ffa6648eaa
정유진 <tntn6981@naver.com>
2025-02-17 01:08:33 +0900

    fix(FE): STT 초기 데이터 출력 형식 변경
    RealtimeNote.jsx 수정 - STT 초기 데이터 출력 형식 변경
    
    #S12P11B203-000
    
    
    Former-commit-id: 02cc0515efc0d66e9629b7ef119563617c159085

--- Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
+++ Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3608f71e087ba7ff74ac35b2e3f218d5036836c85ee9fa0b796a615df79c5456
-size 15246
+oid sha256:1cb8bd51952888b466c5e00e461fa23e6c146cfb857d36a28e88c54f864f15cd
+size 15942

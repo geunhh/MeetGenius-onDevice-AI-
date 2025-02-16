57c8f23eb482a739acc558c14dda6cf3b5857115
rmsgnl11 <rmsgnl11@naver.com>
2025-02-16 23:12:09 +0900

    fix(BE): RAG 데이터 처리 수정
     RAG 데이터 처리 수정 - 에러 해결
    
    #S12P11B203-000
    
    
    Former-commit-id: a7eefcd7308646ba91e3de60e5af58cff1e3a0ef

--- Web/backend/django/meetings/views.py
+++ Web/backend/django/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dde0f1e3a6d798c7258df7b3aaeba794aae96ce2e6a0ec03aeaeba86a34e239c
-size 36152
+oid sha256:e2f265a4ff13a8bd7b1f7e80043c9f32654532af3a9121657bacb6a15dbfe359
+size 36247

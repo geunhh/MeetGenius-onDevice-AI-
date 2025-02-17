e5040fb89ffb47f87747b352895029d01fc688c8
장인영 <dlsdud0704@naver.com>
2025-02-17 13:18:55 +0900

    feat(FE): 요약된 회의록 조회 요약된 회의록 조회 #S12P11B203-300
    Former-commit-id: eb260130f9024a941a1ea32c15ec1974ea4dcfab

--- Web/frontend/react/src/pages/MomSummaryPage.jsx
+++ Web/frontend/react/src/pages/MomSummaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9977004a9f9c4d306118c18f6841484be50652ff191e9666ec18c0e3bdcb402
-size 4614
+oid sha256:f2d26083e810d8c8945a24f88c7e92011732b4dc388d24e0bc99b18eb8418feb
+size 4615

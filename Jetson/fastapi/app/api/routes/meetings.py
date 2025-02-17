8f2a96bddcbf4ccbafc77c3f8c594da4c01fd93d
정찬호 <chano01794@gmail.com>
2025-02-17 14:23:17 +0900

    fix(BE): FastAPI 코드 정리 - routes/meetings.py_02
    FastAPI 코드 정리
    
    #S12P11B203-00
    
    
    Former-commit-id: a39e9a9b151aa77023dafcd6556a5bd7236b797b

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3cea7f3c948611c5af24eb0b5023afc23eb36771326624c7353cc9a8f6b8864
-size 12802
+oid sha256:68a67f806d286ea3225ae89c42eec46482d407beaa8e5b3f4c1092d2a1f77220
+size 12776

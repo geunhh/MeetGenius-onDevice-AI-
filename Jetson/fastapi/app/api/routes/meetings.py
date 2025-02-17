cc58b953fc0a8f66dbbcda89f20f0721c34bad3e
JAEHWAN <woghks1213@gmail.com>
2025-02-18 01:04:19 +0900

    refactor(BE): meetings.prepare_meeting 수정
    모델 로드 백그라운드 스레드로 처리해서 로드 속도 개선
    
    #S12P11B203-302
    
    
    Former-commit-id: 8c5d55910267240ef3f7cb86fbe7fe7d6a6ad53e

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:276fa2d62e85c90a1e303ff18e2a67e6e519482d95a3c741c2a18dc302799938
-size 12842
+oid sha256:37bc76eed7dd658a75a6e78857fbe4261941af4e433d3d3d96b90c5329cabad3
+size 13268

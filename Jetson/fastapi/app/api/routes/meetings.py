66c79022043da1cb02e6e17041e7c58961c07654
정찬호 <chano01794@gmail.com>
2025-02-18 15:35:52 +0900

    fix(BE): FastAPI 버그 수정
    벌레 잡기
    
    #S12P11B203-00
    
    
    Former-commit-id: 18098b30ffa2ff5530415d17f03b51a34f44300c

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4878abef511c10526c2a1c7469d4d69893b572489b75bc5f69b6cae0e25b3b3a
-size 12924
+oid sha256:2778766a543cf5c6f850ce6f33ec8a5253896ef205ca52c25ecb8bf2468d510c
+size 12931

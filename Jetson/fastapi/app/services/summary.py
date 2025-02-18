3a3c3be6ad2e7c25c2554204b20e9b3e5e20a1ad
연재환 <woghks1213@gmail.com>
2025-02-18 13:26:08 +0900

    fix(BE): 회의록 요약 후 모델 언로드
    모델 언로드 추가
    
    #S12P11B203-281
    
    
    Former-commit-id: 562034024a2a12cd232e0724e2663fa202c99eb0

--- Jetson/fastapi/app/services/summary.py
+++ Jetson/fastapi/app/services/summary.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:288ea310d869e791926b21b0b962f048656db7706fa9c797a1a1ecc5559b2e2e
-size 5422
+oid sha256:48946b932aef5d9263c2d80e83bb66fbdf554c123ba833b7c52a44d323d9e4e3
+size 5507

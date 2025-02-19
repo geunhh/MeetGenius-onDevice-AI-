9401621122bead628c6414bda83f9d4636b9fcbe
정유진 <tntn6981@naver.com>
2025-02-19 09:17:33 +0900

    chore(FE):  스타일 수정
    스타일 수정
    
    #S12P11B203-000
    
    
    Former-commit-id: 79b6bee9d0541f01eb5badbbf00c5c4417ab2f66

--- Web/frontend/react/src/components/MeetingRoomBooked.jsx
+++ Web/frontend/react/src/components/MeetingRoomBooked.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9555bb87658286f498ea83ec882ad213fce4285dc5f17101a84583e9b328df73
-size 3420
+oid sha256:12239945c8e135dcc9b38e9f75c2b932f48f393979ab0332125cdb72add9c0f3
+size 6521

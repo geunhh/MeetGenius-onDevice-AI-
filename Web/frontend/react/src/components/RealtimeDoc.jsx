22306ec459185e318ad06c6c02d6c730faa63aa3
장인영 <dlsdud0704@naver.com>
2025-02-19 11:28:53 +0900

    fix(FE): 실시간회의 페이지 SSE hooks 변경 SSE 기능 분리 #S12P11B203-000
    Former-commit-id: e42e113c297845c32cad78eff691cddfa8d6be8a

--- Web/frontend/react/src/components/RealtimeDoc.jsx
+++ Web/frontend/react/src/components/RealtimeDoc.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0844bb986e0498f413d45ff2308e02e6dd78d719d64b8503f635ed70bb26b99
-size 3192
+oid sha256:f3bb3ba4532281be979b5d72c69c5fc42f47ad6dc23a0dce51ac747f76d44184
+size 1738
--- Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
+++ Web/frontend/react/src/pages/RealtimeMeetingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229fbc9ec71db929282295098e94fec70d7e0525de30decb8edf30c37d8fec62
-size 15945
+oid sha256:07da5d693d6810bbcfabe3a76acbbc2faf16b3efe19af34d4ecee789b76945be
+size 14538

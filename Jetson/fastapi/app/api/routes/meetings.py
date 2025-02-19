de24f25694bcaa95aa4106835693a6e28571b7c0
연재환 <woghks1213@gmail.com>
2025-02-19 13:53:07 +0900

    fix(BE): 모델 로드 쓰레드 병렬처리 원복
    버그로 인해 원복 처리
    
    #S12P11B203-00
    
    
    Former-commit-id: cb586877690abd15aa9c2f2f6a8b6a7918e2699c

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b5dba1fc3ab9c393c2621e20b512242b72527bb3f75f0f87160af266adc0cd2
-size 13492
+oid sha256:8a83bab9ff622e1ca0800fe2c34b03b13c4f20c099b66ef5ab97a25c7c6ebfff
+size 12792

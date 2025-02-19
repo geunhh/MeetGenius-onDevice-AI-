b2a4780f40cdc69ba50533f2690166cddac96016
JAEHWAN <woghks1213@gmail.com>
2025-02-19 14:39:13 +0900

    fix(BE): 회의 준비 모델 로드 원복
    모델 로드 순차적 로드로 원복
    
    #S12P11B203-00
    
    
    Former-commit-id: 0a089cfc5f773b47a23ab23c8bcec3bf89267972

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b5dba1fc3ab9c393c2621e20b512242b72527bb3f75f0f87160af266adc0cd2
-size 13492
+oid sha256:5151058f15f9cdf32519f6b454124e6a89cff0fed47550a71ffd7d11c720ca2f
+size 13290

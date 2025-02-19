cb4ba2508b7a0990f1e29dcff1f6b5d9886c43e2
JAEHWAN <woghks1213@gmail.com>
2025-02-19 15:37:49 +0900

    feat(BE): 회의록 요약 전 chromadb 저장
    회의록 요약 전 chromadb 저장
    
    #S12P11B203-00
    
    
    Former-commit-id: 98ce8082310fdd6899788add6544d69e9fca51f1

--- Jetson/fastapi/app/api/routes/meetings.py
+++ Jetson/fastapi/app/api/routes/meetings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcea4d5c133538e4b27d77bed5cb0117f262d2f97a718e253613b28851817140
-size 13329
+oid sha256:4c95aec48f456d965d1753415955cebbe4073740ce9c2ecbe375c3df4b9f18ef
+size 13375

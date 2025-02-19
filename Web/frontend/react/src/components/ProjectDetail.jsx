a8a81c1556eabcc20265bfc45cbc49fcbbec3aee
정유진 <tntn6981@naver.com>
2025-02-19 09:59:37 +0900

    chore(FE):  파일추가 가능 수정
    파일추가 가능 수정
    
    #S12P11B203-000
    
    
    Former-commit-id: 1f3d63362be4d11b0dbab516659d5402d162c373

--- Web/frontend/react/src/components/ProjectDetail.jsx
+++ Web/frontend/react/src/components/ProjectDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c6f3779467feaeca2051e949a7fd3f51da250ad2d4d0e6ad1268fdef510326a
-size 10421
+oid sha256:2f95eb786aa25ad1bce3656b658a2a7d74836a3b46c83092df32819892142e0b
+size 10819

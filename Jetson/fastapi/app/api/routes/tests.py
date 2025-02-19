e6dd5c47249784a2ec770a9ea5bd936bef44b53e
JAEHWAN <woghks1213@gmail.com>
2025-02-19 23:43:09 +0900

    refactor(BE): tests 엔드포인트 리팩토링
    코드 리팩토링
    
    #S12P11B203-00
    
    
    Former-commit-id: f81e136678e0f5e5d03b2106d1cef064c1c82b66

--- Jetson/fastapi/app/api/routes/tests.py
+++ Jetson/fastapi/app/api/routes/tests.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5425c6c7bef850b7f4a63eec26327f44e90ab45c417a38c1dfb90303f505c261
-size 5813
+oid sha256:f2bc59616c9746b23e8b020d8f660e106a9453ab5c020163dbfed8958fb29563
+size 6122

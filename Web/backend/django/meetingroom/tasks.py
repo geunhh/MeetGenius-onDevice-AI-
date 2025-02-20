7da05d13d6645b255eb8a66e224e4e58435f4f9e
geunhh <rmsgnl11@naver.com>
2025-02-20 16:28:43 +0900

    fix(BE): 실시간 회의 페이지 로직 에러 최종 수정
    실시간 회의 페이지 로직 에러최종  수정
    
    #S12P11B203-000
    
    
    Former-commit-id: 423ea94afc0d69d205ebdbd2a55aac4420859b4b

--- Web/backend/django/meetingroom/tasks.py
+++ Web/backend/django/meetingroom/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba448b47f41b0462b84d718b05f9b6f6b2d9d7731946c5d9902409ecc54d1125
-size 5934
+oid sha256:9a9f8d4075730c410963b2a3e8c205cbc390df9f3c44c12690210abb9f98e344
+size 4996

2a58c132c94dcf9dcd9db094be528710327cae88
정찬호 <chano01794@gmail.com>
2025-02-20 16:38:16 +0900

    fix(DA): fix ChromaDB project collection
    크로마DB 관련 조건문 추가
    
    #S12P11B203-00
    
    
    Former-commit-id: e1938cae25ec55cc16362b3c149a28b814108cb5

--- Jetson/fastapi/app/api/routes/projects.py
+++ Jetson/fastapi/app/api/routes/projects.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d8b9ef06095616c74aa4242cbcb290d77e40a81d254c1c8520fdd6dd35a5bd9
-size 5315
+oid sha256:834d8a6f947014fac2efc3975a326819def93aa550007034675cfd1807c2b568
+size 5500

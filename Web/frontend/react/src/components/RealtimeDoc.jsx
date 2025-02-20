15a4c97ee728a054f7d7b229523c032266602a6a
정유진 <tntn6981@naver.com>
2025-02-20 13:21:50 +0900

    chore(FE):  실시간 회의록 SSE연결 수정
    실시간 회의록 SSE연결 수정
    
    #S12P11B203-000
    
    
    Former-commit-id: 5402eab507b99014009140f6a8a7d0e325e15390

--- Web/frontend/react/src/components/RealtimeDoc.jsx
+++ Web/frontend/react/src/components/RealtimeDoc.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fa86dceb82df7dc1ba229f099b826de49d54d6a2c5779db3d6c3a866d536b38
-size 4960
+oid sha256:a3ae96bcd3e117cd343a15eb2fe9ab894fb3376ff8d92445a70039cc9d91a669
+size 2796

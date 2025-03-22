93f235c452047fa7b41170b1f7093c0545e5e5fd
geunhh <rmsgnl11@naver.com>
2025-03-22 13:25:48 +0900

    refactor:회의록 요약 로직 리팩토링

--- Web/backend/django/app/meetingroom/tasks.py
+++ Web/backend/django/app/meetingroom/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a9f8d4075730c410963b2a3e8c205cbc390df9f3c44c12690210abb9f98e344
-size 4996
+oid sha256:aa5da0eb583da2ff66a4d2a0e506e0b1790520a19711f6ce9964ba9190826c78
+size 4840

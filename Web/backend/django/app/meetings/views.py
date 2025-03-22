a7f11c010bf24e79c21637d0a01acd54e5e044a1
geunhh <rmsgnl11@naver.com>
2025-03-22 13:21:58 +0900

    refactor:meeting logic refactoring

--- Web/backend/django/app/meetings/views.py
+++ Web/backend/django/app/meetings/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a54f459cc564f3a18759360a74247815f55bda32a46dc0181ab1f9b21f7ad1ee
-size 37350
+oid sha256:f1b69619304c8f11317f1919acafd2ee51905accd2683c3190bc10427ddeaf73
+size 33228

1fdfd2425d46b22949c8303ee184dbd9c5a20b23
geunhh <rmsgnl11@naver.com>
2025-03-22 13:23:51 +0900

    refactor:프로젝트 파일업로드 로직 리팩토링

--- Web/backend/django/app/projects/tasks.py
+++ Web/backend/django/app/projects/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8713cbf24c71f3660f13ea23aa4840a509f9df09f5bb1dd292d8f4c1afdb5507
-size 3536
+oid sha256:92b30efe8883bd314f6e78c78094a278add809da38b000344e69781acf3a1479
+size 3537
